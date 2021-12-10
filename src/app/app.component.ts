e6980ae27480fbce4ea7f25a685a581197b639bd
ahmed3bdelnour <ahmed.i@trufla.com>
2021-12-10 03:36:57 +0200

    - add more types and refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-//@ts-nocheck
 import { Component } from '@angular/core';
+
+enum Player {
+  X = 'X',
+  O = 'O',
+}
+
+type Winner = Player | 'None';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +13,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  boardWidth;
-  board;
-  firstPlayer;
-  secondPlayer;
-  nextPlayer;
-  winner;
+  boardWidth: number;
+  board: string[][];
+  firstPlayer: Player;
+  secondPlayer: Player;
+  nextPlayer: Player;
+  winner: Winner;
 
-  ngOnInit() {
-    this.resetGame();
+  constructor() {
+    this.initOrResetGame();
   }
 
-  resetGame() {
+  initOrResetGame(): void {
     this.boardWidth = 3;
 
     this.board = [];
@@ -27,76 +33,116 @@ export class AppComponent {
       this.board[i] = [];
     }
 
-    this.firstPlayer = 'X';
-    this.secondPlayer = 'O';
+    this.firstPlayer = Player.X;
+    this.secondPlayer = Player.O;
     this.nextPlayer = this.firstPlayer;
     this.winner = 'None';
   }
 
-  handleClickCell(rowIndex, colIndex) {
-    if (!!this.board[rowIndex][colIndex]) return;
+  handleClickCell(rowIndex: number, colIndex: number): void {
+    if (!!this.getCellValue(rowIndex, colIndex)) return;
 
     this.board[rowIndex][colIndex] = this.nextPlayer;
-    this.setWinner();
+
+    this.winner = this.getWinner();
+
     this.nextPlayer =
       this.nextPlayer === this.firstPlayer
         ? this.secondPlayer
         : this.firstPlayer;
   }
 
-  setWinner() {
-    if (this.checkRowsAndColumns() || this.checkDiagonals()) {
-      this.winner = this.nextPlayer;
-    } else {
-      this.winner = 'None';
-    }
+  getCellValue(rowIndex: number, colIndex: number): Player{
+    return this.board[rowIndex][colIndex] as Player;
   }
 
-  checkRowsAndColumns() {
+  getWinner(): Winner {
+    let winner: Winner;
+
+    if (this.isAnyRowOrColumnCompleted() || this.isAnyDiagonalCompleted()) {
+      winner = this.nextPlayer as Winner;
+    } else {
+      winner = 'None';
+    }
+
+    return winner;
+  }
+
+  isAnyRowOrColumnCompleted(): boolean {
     for (let x = 0; x < this.boardWidth; x++) {
-      const rowPoints = [];
-      const columnPoints = [];
+      const { rowCells, columnCells } =
+        this.getCellsValuesForRowAndColumnByIndex(x);
 
-      for (let y = 0; y < this.boardWidth; y++) {
-        rowPoints.push(this.board[x][y]);
-        columnPoints.push(this.board[y][x]);
-      }
+      const isRowCompleted = this.isCompleted(rowCells);
 
-      const completeEntryForFirstPlayer = this.firstPlayer.repeat(
-        this.boardWidth
-      );
-      const completeEntryForSecondPlayer = this.secondPlayer.repeat(
-        this.boardWidth
-      );
+      if (isRowCompleted) return isRowCompleted;
 
-      const rowConcatenation = this.concatPoints(rowPoints);
+      const isColumnCompleted = this.isCompleted(columnCells);
 
-      const rowMatches =
-        rowConcatenation === completeEntryForFirstPlayer ||
-        rowConcatenation === completeEntryForSecondPlayer;
-
-      if (rowMatches) return rowMatches;
-
-      const columnConcatenation = this.concatPoints(columnPoints);
-
-      const columnMatches =
-        columnConcatenation === completeEntryForFirstPlayer ||
-        columnConcatenation === completeEntryForSecondPlayer;
-
-      if (columnMatches) return columnMatches;
+      if (isColumnCompleted) return isColumnCompleted;
     }
 
     return false;
   }
 
-  checkDiagonals() {
-    const firstDiagonalPoints = [];
-    const secondDiagonalPoints = [];
+  isAnyDiagonalCompleted(): boolean {
+    const { firstDiagonalCells, secondDiagonalCells } =
+      this.getCellsValuesForDiagonals();
+
+    const isFirstDiagonalCompleted = this.isCompleted(firstDiagonalCells);
+
+    if (isFirstDiagonalCompleted) return isFirstDiagonalCompleted;
+
+    const isSecondDiagonalCompleted = this.isCompleted(secondDiagonalCells);
+
+    return isSecondDiagonalCompleted;
+  }
+
+  concatCells(cells: string[]): string {
+    return cells.reduce((acc, current) => {
+      acc = acc + current;
+      return acc;
+    });
+  }
+
+  getCellsValuesForRowAndColumnByIndex(index: number): {
+    rowCells: Player[];
+    columnCells: Player[];
+  } {
+    const rowCells: Player[] = [];
+    const columnCells: Player[] = [];
+
+    for (let y = 0; y < this.boardWidth; y++) {
+      rowCells.push(this.board[index][y] as Player);
+      columnCells.push(this.board[y][index] as Player);
+    }
+
+    return {
+      rowCells,
+      columnCells,
+    };
+  }
+
+  getCellsValuesForDiagonals(): {
+    firstDiagonalCells: Player[];
+    secondDiagonalCells: Player[];
+  } {
+    const firstDiagonalCells: Player[] = [];
+    const secondDiagonalCells: Player[] = [];
 
     for (let x = 0, y = this.boardWidth - 1; x < this.boardWidth; x++, y--) {
-      firstDiagonalPoints.push(this.board[x][x]);
-      secondDiagonalPoints.push(this.board[x][y]);
+      firstDiagonalCells.push(this.board[x][x] as Player);
+      secondDiagonalCells.push(this.board[x][y] as Player);
     }
+
+    return {
+      firstDiagonalCells,
+      secondDiagonalCells,
+    };
+  }
+
+  isCompleted(cells: string[]): boolean {
+    const CellsConcatenation = this.concatCells(cells);
 
     const completeEntryForFirstPlayer = this.firstPlayer.repeat(
       this.boardWidth
@@ -105,26 +151,9 @@ export class AppComponent {
       this.boardWidth
     );
 
-    const firstDiagonalConcatenation = this.concatPoints(firstDiagonalPoints);
-
-    const firstDiagonalMatches =
-      firstDiagonalConcatenation === completeEntryForFirstPlayer ||
-      firstDiagonalConcatenation === completeEntryForSecondPlayer;
-
-    if (firstDiagonalMatches) return firstDiagonalMatches;
-
-    const secondDiagonalConcatenation = this.concatPoints(secondDiagonalPoints);
-
     return (
-      secondDiagonalConcatenation === completeEntryForFirstPlayer ||
-      secondDiagonalConcatenation === completeEntryForSecondPlayer
+      CellsConcatenation === completeEntryForFirstPlayer ||
+      CellsConcatenation === completeEntryForSecondPlayer
     );
-  }
-
-  concatPoints(points) {
-    return points.reduce((acc, cur) => {
-      acc = acc + cur;
-      return acc;
-    });
   }
 }
